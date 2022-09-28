302f3a75b4037f94f76412c6dbe520c8e9be2534
Md Rahamat Ullah <43946975+mdrahama@users.noreply.github.com>
2022-09-28 16:34:03 -0400

    Update Inventory_Visual.py

--- Inventory_Visual.py
+++ Inventory_Visual.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plot
 import seaborn as sb
 import datetime as dt
 
-%matplotlib inline
+#%matplotlib inline
 os.chdir("C:\\Users\\ullahm2\\Downloads") 
 cwd = os.getcwd() 
 print (cwd)
